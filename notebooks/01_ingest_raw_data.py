60f57dd98e64b70537ed7598530e2551d78bcd34
Appy Tong <appy@Appys-MacBook-Pro-2.local>
2025-11-28 12:18:25 +0100

    removed python files

--- notebooks/01_ingest_raw_data.py
+++
@@ -1,7 +0,0 @@
-raw_df = spark.read.csv("/Workspace/Repos/appytong@gmail.com/learning-project/data/raw/dataset.csv", header=True, inferSchema=True)
-
-raw_df.printSchema()
-raw_df.limit(5).display()
-raw_df.count()
-
-raw_df.write.saveAsTable("workspace.default.raw_table")
